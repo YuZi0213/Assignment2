9d98367363354e05e9e122c62f70100d8aae5b63
YuZi0213 <77252090+YuZi0213@users.noreply.github.com>
2022-04-04 17:40:54 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -330,6 +330,49 @@ int main(){
         }
     }
 
+    // Part 3 - case 3
+    {
+        assign2_exception::exception e = 0;
+        try {
+                BST *bst = new BST;
+                bst->comp = comp;
+                bst->root = nullptr;
+                tree_node *targetnode;
+                for (int i = 10000000; i >= 1; i--){
+                    insert_into_BST(bst, i, &targetnode);
+                }
+
+                // BST *new_bst = new BST;
+                // new_bst->root = targetnode;
+                // new_bst->comp = comp;
+                // insert_into_BST(new_bst, )
+                std::string *s1 = new std::string, *s2 = new std::string;
+                preorder(bst, s1);
+ 
+                inorder(bst, s2);
+
+                std::string ans1="5011432152911363355389015611110033";
+                std::string ans2="2911321536335011455386111901510033";
+                int tree_c=0;
+                if (!judge_bst(bst,tree_c)){
+                    delete s1;
+                    delete s2;
+                    delete_tree(bst->root);
+                    delete bst;
+                    
+                    throw e;
+                }
+                count++;
+                delete s1;
+                delete s2;
+                delete_tree(bst->root);
+                delete bst;
+        }
+        catch(assign2_exception::exception){
+            std::cout<<"\033[41;11m Error in part 3, case3! \033[0m\n";
+        }
+    }
+
     /*
         This is the test case for part 4.
     */
@@ -382,6 +425,7 @@ int main(){
                 if (*target_node==NULL || (*target_node)->data !=32) delete_tree(bst->root), delete bst, e = 8, throw e; // judgement step 8.
                 count++;
                 delete_tree(bst->root);
+                delete bst; 
         }
         catch(assign2_exception::exception){
             std::cout<<"\033[41;11m Error in part 4, case1! Happen in judgement step "<<e<<". \033[0m\n";
@@ -438,12 +482,13 @@ int main(){
                 if (!judge_bst(bst, tree_c) || bst->root!=test_node_three) delete_tree(bst->root), delete bst, throw e=3;
                 count++;
                 delete_tree(bst->root);
+                delete bst; 
         }
         catch(assign2_exception::exception){
             std::cout<<"\033[41;11m Error in part 5, case1! Happen in judgement step "<<e<<". \033[0m\n";
         }
     }
-    std::cout<<"The final result is "<<count<<"/22\n";
+    std::cout<<"The final result is "<<count<<"/23\n";
     std::cout<<"If you have patience, you can read my code and add some new test cases.\n";
     std::cout<<"Hope you a good score!\n";
     std::cout<<"If you find any bug or any wrong answer, please contact us.\n";
@@ -570,6 +615,6 @@ void delete_tree(tree_node * node){
     author: 
         灵犀
     co-author:
-        方艺钧
+        方艺钧、邓值仁
 */
 
